1688ffcfb19123d466b3b9bfb4bce53761c79f67
Biggi <adis3778@zohomail.in>
2025-11-12 15:39:59 +0530

    4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "750521338"
+    serviceWorkerVersion: "4066236135"
   }
 });
