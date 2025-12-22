8173e92d81fe321a60804560811aa515b454d18e
RenoResearch <renoresearch0201@gmail.com>
2025-12-22 08:18:22 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3452347178"
+    serviceWorkerVersion: "4037386299"
   }
 });
