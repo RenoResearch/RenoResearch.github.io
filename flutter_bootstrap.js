9be6e976bdadc02299f9d10feec419dd4303538f
edtech-hub <adis3778@zohomail.in>
2026-01-25 15:31:35 +0800

    Deploy build 2026.01.25.1529 - Package navigation fix, badge styling improvements

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "317151351"
+    serviceWorkerVersion: "248024849"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e0deb6db9ac353f5f6f692a1f530ba00",
-"version.json": "7cc2f5d927ea626c918725318acf7756",
+const RESOURCES = {"flutter_bootstrap.js": "e70dae9f376b2a6f835f24605390c1ae",
+"version.json": "7e5571b31e4bc35da1a424849052e646",
 "index.html": "7abeac3cdee4e32e7e65bdb74944f879",
 "/": "7abeac3cdee4e32e7e65bdb74944f879",
-"main.dart.js": "12afd536257247a207cbcdc29a555ab4",
+"main.dart.js": "d944188ef000653e1da6fe822cab2640",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
