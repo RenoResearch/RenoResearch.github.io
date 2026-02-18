7317a929a63fa838af86f8b4f8476938cc21fe79
edtech-hub <adis3778@zohomail.in>
2026-02-19 01:57:10 +0800

    Deploy build 2026.02.19.0155 - Add bundle view route

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6bdeb3287180a35b1e7126a540825ce1",
-"version.json": "accc3a1fa6f4b7357d7c43708c807350",
+const RESOURCES = {"flutter_bootstrap.js": "f0b144d664026b18da64c2da9ff9eaee",
+"version.json": "e9fb94fe990541f4eec87cdb5f05e3bf",
 "index.html": "7abeac3cdee4e32e7e65bdb74944f879",
 "/": "7abeac3cdee4e32e7e65bdb74944f879",
-"main.dart.js": "8dd8927c9e1816c371382958a3c31d57",
+"main.dart.js": "b0704c20b16e09af0e2dbcec4b865400",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
