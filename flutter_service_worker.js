b1b7a4dd5651ec7b056e6527971ccafbafb5ced7
edtech-hub <adis3778@zohomail.in>
2026-02-24 16:28:42 +0800

    Deploy build 2026.02.24.1627 - Production release

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9af566a9569cad0c262fe753da02c9d9",
-"version.json": "2e0deef36f8f9bf01fc8d0aada334d52",
+const RESOURCES = {"flutter_bootstrap.js": "6668f4f9956052ea2c2344f958999ee0",
+"version.json": "24957a1b53fbc98a331445daffdfc594",
 "index.html": "7abeac3cdee4e32e7e65bdb74944f879",
 "/": "7abeac3cdee4e32e7e65bdb74944f879",
-"main.dart.js": "c5302c971e870c1e92bd6af879049904",
+"main.dart.js": "77dffd7ea9a99a1913dd70b74443d31f",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
